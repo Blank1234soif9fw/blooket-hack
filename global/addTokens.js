3d6fb0be7eb42eb0b03b18f796df4091f244966f
Blank1234soif9fw <94761020+Blank1234soif9fw@users.noreply.github.com>
2021-12-04 07:57:37 -0800

    Update addTokens.js
    pls make this real

--- global/addTokens.js
+++ global/addTokens.js
@@ -6,10 +6,10 @@ async function getName(authToken) {
 };
 
 async function addCurrencies() {
-    const tokens = Number(prompt('How many tokens do you want to add to your account? (500 daily)'));
+    const tokens = Number(prompt('How many tokens do you want to add to your account? (1500 daily)'));
     const myToken = localStorage.token.split('JWT ')[1];
 
-    if (tokens > 500) {
+    if (tokens > 1500) {
         alert('You can only add up to 500 tokens daily.')
     }
 
@@ -22,7 +22,7 @@ async function addCurrencies() {
         },
         body: JSON.stringify({
             addedTokens: tokens,
-            addedXp: 300,
+            addedXp: 500,
             name: await getName(myToken)
         })
     });
